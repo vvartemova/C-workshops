a94bb30382f8f8a73103661bf9e42385d57bd736
Victoria <vvartemova@yandex.ru>
2022-06-09 17:51:30 +0300

    Файлы 3-го задания добавлены в отслеживаемые

---
+++ wokshop001/task3/Program.cs
@@ -0,0 +1,21 @@
+﻿//Напишите программу, которая на вход принимает число и выдаёт,
+// является ли число чётным (делится ли оно на два без остатка)
+
+Console.Write ("Введите число  - ");
+string paramString = Console.ReadLine();
+int paramInt = int.Parse (paramString);
+int result = paramInt%2;
+
+if (result == 0)
+{
+Console.WriteLine($"Число {paramInt} - четное");
+} 
+else 
+{
+    Console.WriteLine($"Число {paramInt} - нечетное");
+    }
+
+
+
+
+
