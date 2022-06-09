20d850a761b9015f0510814db662cfb625bd660c
Victoria <vvartemova@yandex.ru>
2022-06-09 17:30:26 +0300

    Выполнено задание 2 семинара 1

---
+++ wokshop001/task2/Program.cs
@@ -0,0 +1,17 @@
+﻿//Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел
+
+Console.Write ("Введите число 1 - ");
+string paramStringA = Console.ReadLine();
+int paramIntA = int.Parse (paramStringA);
+Console.Write ("Введите число 2 - ");
+string paramStringB = Console.ReadLine();
+int paramIntB = int.Parse (paramStringB);
+Console.Write ("Введите число 3 - ");
+string paramStringС = Console.ReadLine();
+int paramIntС = int.Parse (paramStringС);
+int max = paramIntA;
+
+if (paramIntB > max) max = paramIntB;
+if (paramIntС > max) max = paramIntС;
+Console.WriteLine($"Максимальное число равно {max}");
+
