29581eabef7c841c43e83c05af9e014bdc76d930
Victoria <vvartemova@yandex.ru>
2022-06-09 17:21:31 +0300

    Выполнено 1 задание 1 семинара

---
+++ wokshop001/task1/Program.cs
@@ -0,0 +1,20 @@
+﻿//Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+
+Console.Write ("Введите число 1 - ");
+string paramStringA = Console.ReadLine();
+int paramIntA = int.Parse (paramStringA);
+Console.Write ("Введите число 2 - ");
+string paramStringB = Console.ReadLine();
+int paramIntB = int.Parse (paramStringB);
+
+if (paramIntA > paramIntB) 
+{ 
+    Console.WriteLine ($"Большее число - {paramIntA}");
+    Console.WriteLine ($"Меньшее число - {paramIntB}");
+}
+else 
+{   Console.WriteLine ($"Большее число - {paramIntB}");
+    Console.WriteLine ($"Меньшее число - {paramIntA}");
+
+}
+
