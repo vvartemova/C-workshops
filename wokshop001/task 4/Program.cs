a563a1c89b3c3ce48f5aaac22a01495977455513
Victoria <vvartemova@yandex.ru>
2022-06-09 18:30:07 +0300

    Выполнено задание 4 семинара 1

---
+++ wokshop001/task 4/Program.cs
@@ -0,0 +1,16 @@
+﻿//Напишите программу, которая на вход принимает число (N), 
+//а на выходе показывает все чётные числа от 1 до N.
+
+Console.Write ("Введите число  - ");
+string paramString = Console.ReadLine();
+int paramInt = int.Parse (paramString);
+int i = 1;
+
+while (i <= paramInt)
+{ int result = i%2;
+  if  (result == 0) 
+  {
+    Console.WriteLine(i);
+  }
+  i = i+1;
+}
