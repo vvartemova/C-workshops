ca323a24435f8b965ba3dc6b53d328e274875125
Victoria <vvartemova@yandex.ru>
2022-06-13 13:44:07 +0300

    Выполнено задание 1 семинара 2

---
+++ workshop002/Task1/Program.cs
@@ -0,0 +1,25 @@
+﻿// Напишите программу, которая принимает на вход трехзначное число 
+//и на выходе показывает вторую цифру этого числа
+
+int prompt (string message)
+{
+    Console.Write(message);
+    string number = Console.ReadLine();
+    int numberInt = int.Parse (number);
+    return numberInt;
+    }
+
+int numberInt = prompt ("Введите трехзначное число ");
+
+int [] array = new int[3];
+int index = array.Length - 1;
+
+while (numberInt > 0)
+{
+array[index] = numberInt % 10;
+numberInt = numberInt / 10;
+index--;
+}
+
+Console.WriteLine (array [1]);
+
