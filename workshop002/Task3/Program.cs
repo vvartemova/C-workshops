9be565855cbd9edcfd742748deee34709f8eeb99
Victoria <vvartemova@yandex.ru>
2022-06-13 14:30:44 +0300

    Выполнено задание 3,4 семинара 2

---
+++ workshop002/Task3/Program.cs
@@ -0,0 +1,22 @@
+﻿//Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет
+
+Console.Write("Введите число: ");
+string number = Console.ReadLine();
+int numberInt = int.Parse(number);
+
+if (number.Length < 3) Console.WriteLine ("Третьей цифры нет");
+
+else {
+int [] array = new int[number.Length];
+int index = array.Length - 1;
+
+while (numberInt > 0)
+{
+array[index] = numberInt % 10;
+numberInt = numberInt / 10;
+index--;
+}
+
+Console.WriteLine (array [2]);
+}
+
---
+++ workshop002/Task4/Program.cs
@@ -0,0 +1,8 @@
+﻿//Напишите программу, которая принимает на вход цифру, 
+//обозначающую день недели и проверяет, является ли этот день выходным"
+Console.Write("Введите цифру дня недели: ");
+string number = Console.ReadLine();
+int numberInt = int.Parse(number);
+
+if (numberInt == 6 || numberInt==7 ) Console.WriteLine ("Да");
+else Console.WriteLine ("Нет");
