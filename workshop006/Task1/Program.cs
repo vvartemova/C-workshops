3ce23fc0cddaad4bc953c0c20be1d2dd1ae3b9bd
Victoria <vvartemova@yandex.ru>
2022-06-27 22:39:39 +0300

    Выполнены задания 6 семинара

---
+++ workshop006/Task1/Program.cs
@@ -0,0 +1,29 @@
+﻿// Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+int Prompt(string message)
+{
+    Console.Write(message);
+    string number = Console.ReadLine();
+    int numInt = int.Parse(number);
+    return numInt;
+}
+
+void printArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        System.Console.Write(array [i] + " ");
+    }
+    Console.WriteLine();
+}
+
+int m = Prompt ("Укажите количество чисел, которое необходимо ввести => ");
+int [] numbers = new int [m];
+int countPositive = 0;
+for (int i=0; i < m; i++) 
+{
+    numbers [i] = Prompt ("Введите число => ");
+    if (numbers [i] > 0) countPositive++;
+}
+printArray(numbers);
+Console.WriteLine ("Количество чисел больше нуля равно "+ countPositive);
---
+++ workshop006/Task2/Program.cs
@@ -0,0 +1,25 @@
+﻿// Напишите программу, которая найдёт точку пересечения двух прямых, заданных 
+//уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+
+int Prompt(string message)
+{
+    Console.Write(message);
+    string number = Console.ReadLine();
+    int numInt = int.Parse(number);
+    return numInt;
+}
+
+int k1 = Prompt ("Введите значение k1 => ");
+int b1 = Prompt("Введите значение b1 => ");
+int k2 = Prompt("Введите значение k2 => ");
+int b2 = Prompt("Введите значение b2 => ");
+
+if (k1 == k2) 
+{
+    Console.WriteLine($"Прямые не пересекаются");
+    Environment.Exit(9);
+}
+int x = (b2-b1)/(k1-k2);
+int y = k1*x + b1;
+
+Console.WriteLine ($"Прямые пересекаются в точке с координатами {x} ; {y}");
