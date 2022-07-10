3b3cf61df783f81edf58fddbd80e3a3a26509523
Victoria <vvartemova@yandex.ru>
2022-07-10 23:02:22 +0300

    Выполнены задания 9 семинара

---
+++ workshop009/Task1/Program.cs
@@ -0,0 +1,29 @@
+﻿//Задайте значения M и N. Напишите программу, которая выведет все чётные натуральные числа 
+//в промежутке от M до N с помощью рекурсии
+
+
+int Prompt(string message)
+{
+    System.Console.Write(message);
+    return int.Parse(Console.ReadLine());
+}
+
+void PrintEven(int M, int N)
+{
+    if (M > N)
+    {
+        return;
+    }
+
+    if (M % 2 == 0)
+    {
+        Console.Write($"{M} ");
+    }
+
+    PrintEven(M + 1, N);
+
+}
+int M = Prompt("Введите M => ");
+int N = Prompt("Введите N => ");
+PrintEven(M, N);
+Console.WriteLine();
---
+++ workshop009/Task2/Program.cs
@@ -0,0 +1,23 @@
+﻿// Задайте значения M и N. Напишите программу, которая найдёт сумму 
+//натуральных элементов в промежутке от M до N с помощью рекурсии.
+
+
+int Prompt(string message)
+{
+    System.Console.Write(message);
+    return int.Parse(Console.ReadLine());
+}
+
+int PrintSumNatural(int M, int N)
+{
+    if (M > N)
+    {
+        return 0;
+    }
+
+    return PrintSumNatural(M + 1, N) + M;
+
+}
+int M = Prompt("Введите M => ");
+int N = Prompt("Введите N => ");
+Console.WriteLine(PrintSumNatural(M, N));
